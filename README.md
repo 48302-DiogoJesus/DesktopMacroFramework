9fb5ded7888cad9a8292805c7d8e9e4f830a5e94
ist1111266 <diogofjesus@tecnico.ulisboa.pt>
2023-11-04 16:40:43 +0000

    Add setup.py

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 - Monitor window to visualize macro in real-time
   - start/pause/resume/stop macro
   - schedule macro execution for later
+
+Install library
+
+`pip install git+https://github.com/48302-DiogoJesus/DesktopMacroFramework`
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+# Read requirements from requirements.txt
+with open('requirements.txt', 'r') as file:
+    requirements = file.read().splitlines()
+
+print("Requirements HE", requirements)
+
+setup(
+    name='DesktopAutomationFramework', 
+    version='0.0.1',  
+    requirements=requirements
+)
