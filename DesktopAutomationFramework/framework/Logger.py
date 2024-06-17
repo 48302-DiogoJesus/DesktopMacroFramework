00edc855504ef8e54ad0cb415b31b866a3267d81
Diogo Jesus <72945621+48302-DiogoJesus@users.noreply.github.com>
2024-06-17 18:57:53 +0100

    Update logger to use local timezone

--- DesktopAutomationFramework/framework/Logger.py
+++ DesktopAutomationFramework/framework/Logger.py
@@ -1,25 +1,29 @@
-import datetime
 import os
+import datetime
+import pytz
 
 LOGS_FOLDER_NAME = "logs"
 
 class Logger:
     log_file_name = ""
     logs_path = ""
+    local_timezone = pytz.timezone('local')  # Get local timezone of the machine
 
     def __init__(self, macro_path: str):
         self.macro_path = macro_path
         self.new_file()
 
     def new_file(self):
-        self.log_file_name = datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S") + ".txt"
+        now = datetime.datetime.now(self.local_timezone)  # Get current datetime in local timezone
+        self.log_file_name = now.strftime("%Y-%m-%d %H.%M.%S") + ".txt"
 
-        self.logs_path = self.macro_path + "/" + LOGS_FOLDER_NAME
+        self.logs_path = os.path.join(self.macro_path, LOGS_FOLDER_NAME)
         if not os.path.exists(self.logs_path):
             os.makedirs(self.logs_path)
 
     def write(self, log_content: str):
-        log_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        now = datetime.datetime.now(self.local_timezone)  # Get current datetime in local timezone
+        log_time = now.strftime("%Y-%m-%d %H:%M:%S")
         
         file_path = os.path.join(self.logs_path, self.log_file_name)
 
@@ -27,7 +31,8 @@ class Logger:
             file.write(f"[{log_time}] {log_content}\n")
 
     def error(self, error_msg: str):
-        log_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        now = datetime.datetime.now(self.local_timezone)  # Get current datetime in local timezone
+        log_time = now.strftime("%Y-%m-%d %H:%M:%S")
         
         file_path = os.path.join(self.logs_path, self.log_file_name)
 
